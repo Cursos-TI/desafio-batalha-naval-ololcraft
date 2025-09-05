e300a311c03b03316b4d45dce6afa9a51e9b1517
ololcraft <huehueguy01@gmail.com>
2025-09-05 23:57:55 +0000

    copie e colei do meu estudio

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,111 @@
 #include <stdio.h>
+int main(){
+    int tabuleiro[10][10];
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+    for (int i = 0; i < 10; i++)
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            if (i == 0 && x > 1 && x < 5)
+            {
+                tabuleiro[i][x] = 3;
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+            }else if (i > 5 && i < 9 && x == 8)
+            {
+                tabuleiro[i][x] = 3;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+            }else{
+                tabuleiro[i][x] = 0;
+                
+            }
+        }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    }
+    tabuleiro[2][1] = 3;
+    tabuleiro[3][2] = 3;
+    tabuleiro[4][3] = 3;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    tabuleiro[7][5] = 3;
+    tabuleiro[8][4] = 3;
+    tabuleiro[9][3] = 3;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            printf(" %d", tabuleiro[i][x]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int cone[3][5];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if ((i == 0 && j ==2) || (i == 1 && j > 0 && j < 4) || (i == 2))
+            {
+                cone[i][j] = 1;
+            }else{
+                cone[i][j] = 0;
+            }            
+        }
+        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf(" %d", cone[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int cruz[3][5];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+
+            if (j == 2 || i == 1)
+            {
+                cruz[i][j]=1;
+            }else{
+                cruz[i][j]=0;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf(" %d", cruz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    int octa[3][5];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if ((i==1 && j>0 && j<4) || j==2)
+            {
+                octa[i][j]=1;
+            }else{
+                octa[i][j]=0;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf(" %d", octa[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
